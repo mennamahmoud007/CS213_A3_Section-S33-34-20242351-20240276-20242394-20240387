ac704b01163245de9a07a524e67ccf6832a9d190
شروق هاني صبري 20240276 <shroukhani240@gmail.com>
2025-12-01 19:26:44 +0200

    Add FourInARow folder with game files

---
+++ FourInARow/FourInARow_Board.cpp
@@ -0,0 +1,82 @@
+﻿#include "FourInARow_Board.h"
+#include <iostream>
+using namespace std;
+
+FourInARow_Board::FourInARow_Board()
+    : Board<char>(6, 7) // 6 صفوف و7 أعمدة
+{
+    // تهيئة اللوحة بالفراغات
+    for (int i = 0; i < rows; ++i)
+        for (int j = 0; j < columns; ++j)
+            board[i][j] = ' ';
+}
+
+FourInARow_Board::~FourInARow_Board() {}
+
+bool FourInARow_Board::update_board(Move<char>* move) {
+    int col = move->get_y();
+    char symbol = move->get_symbol();
+
+    
+    for (int row = rows - 1; row >= 0; --row) {
+        if (board[row][col] == ' ') {
+            board[row][col] = symbol;
+            ++n_moves;
+            return true;
+        }
+    }
+    // العمود ممتلئ
+    return false;
+}
+
+bool FourInARow_Board::check_four_in_a_row(char symbol) {
+ 
+    for (int i = 0; i < rows; ++i)
+        for (int j = 0; j <= columns - 4; ++j)
+            if (board[i][j] == symbol && board[i][j + 1] == symbol &&
+                board[i][j + 2] == symbol && board[i][j + 3] == symbol)
+                return true;
+
+   
+    for (int i = 0; i <= rows - 4; ++i)
+        for (int j = 0; j < columns; ++j)
+            if (board[i][j] == symbol && board[i + 1][j] == symbol &&
+                board[i + 2][j] == symbol && board[i + 3][j] == symbol)
+                return true;
+
+    for (int i = 0; i <= rows - 4; ++i)
+        for (int j = 0; j <= columns - 4; ++j)
+            if (board[i][j] == symbol && board[i + 1][j + 1] == symbol &&
+                board[i + 2][j + 2] == symbol && board[i + 3][j + 3] == symbol)
+                return true;
+
+    for (int i = 3; i < rows; ++i)
+        for (int j = 0; j <= columns - 4; ++j)
+            if (board[i][j] == symbol && board[i - 1][j + 1] == symbol &&
+                board[i - 2][j + 2] == symbol && board[i - 3][j + 3] == symbol)
+                return true;
+
+    return false;
+}
+
+bool FourInARow_Board::is_win(Player<char>* player) {
+    return check_four_in_a_row(player->get_symbol());
+}
+
+bool FourInARow_Board::is_lose(Player<char>* player) {
+  
+    char opponent_symbol = (player->get_symbol() == 'X') ? 'O' : 'X';
+    return check_four_in_a_row(opponent_symbol);
+}
+
+bool FourInARow_Board::is_draw(Player<char>* player) {
+    return n_moves == rows * columns && !is_win(player) && !is_lose(player);
+}
+
+bool FourInARow_Board::game_is_over(Player<char>* player) {
+    return is_win(player) || is_lose(player) || is_draw(player);
+}
+
+vector<vector<char>>& FourInARow_Board::get_board_matrix() {
+    return board;
+}
---
+++ FourInARow/FourInARow_Board.h
@@ -0,0 +1,30 @@
+﻿#ifndef FOURINAROW_BOARD_H
+#define FOURINAROW_BOARD_H
+
+#include "BoardGame_Classes.h"
+#include <vector>
+#include <iostream>
+using namespace std;
+
+class FourInARow_Board : public Board<char> {
+private:
+    vector<vector<char>> board_matrix;
+    const int rows = 6;
+    const int cols = 7;
+    const char blank = ' ';
+
+    bool check_four_in_a_row(char symbol);
+
+public:
+    FourInARow_Board();
+    ~FourInARow_Board();
+
+    vector<vector<char>>& get_board_matrix();
+    bool update_board(Move<char>* move);
+    bool is_win(Player<char>* player);
+    bool is_lose(Player<char>* player);
+    bool is_draw(Player<char>* player);
+    bool game_is_over(Player<char>* player);
+};
+
+#endif
